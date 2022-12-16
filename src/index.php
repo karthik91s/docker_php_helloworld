f3c8756a425e1d071a2e4ecc7ccb2235549d5683
karthik91s <46275205+karthik91s@users.noreply.github.com>
2022-12-16 20:15:28 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,39 @@
 <?php
 
+
 echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "
+echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "
+  echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
+echo "Hello 16th Dec 2022!  <br>"
+echo "All is Well "echo "Hello, Welcome to karthik Docker house! <br>";
 echo "Hello 16th Dec 2022!  <br>"
 echo "All is Well "
 ?>
