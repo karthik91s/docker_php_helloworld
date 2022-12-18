9d14556f75a3a247a29b1bbf09439a2656bd4e6e
karthik91s <46275205+karthik91s@users.noreply.github.com>
2022-12-18 18:57:00 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,6 @@
 <?php
 echo "Hello, Welcome to karthik Docker house! <br>";
 echo "Hello 16th Dec 2022!  <br>";
-echo "SCM Build successful "
-  echo "SCM Build successful "
-  echo "SCM Build successful "
-  echo "SCM Build successful "
-  echo "SCM Build successful "
+?>
 
 
